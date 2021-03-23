7002aecf12a862cbce844a3bfe763cef2591ae5a
Laura Ochoa <Iki.ochoa@gmail.com>
2021-03-23 09:01:46 +0000

    Finished course

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -7,7 +7,7 @@ with payments as (
         status,
 
         -- amount is stored in cents, convert to dollar
-        amount / 100 as amount, 
+        {{ cents_to_dollars('amount') }} as amount, 
         created as created_at
         
     from {{ source('stripe','payment') }}
